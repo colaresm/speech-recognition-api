7877c0bf2fbb7a0988ab4fb5cfd66ee58541c73c
Marcelo Colares <marcelocolares@MacBookAir.lan>
2026-01-02 16:58:38 -0300

    Update Readme

--- api/database_layer/database.py
+++ api/database_layer/database.py
@@ -1,10 +1,7 @@
 import os
-from dotenv import load_dotenv
 from pymongo import MongoClient
 
 def connection():
-    #load_dotenv()
-
     mongo_uri = os.getenv("MONGO_URI")
     mongo_db = os.getenv("MONGO_DB")
     
