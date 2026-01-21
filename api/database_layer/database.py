7a0e183e2f6e58855cb06af48cf33360dfd2c018
Marcelo Colares <marcelocolares@MacBookAir.lan>
2026-01-20 22:20:24 -0300

    fix docker compose

--- api/database_layer/database.py
+++ api/database_layer/database.py
@@ -4,7 +4,6 @@ from pymongo import MongoClient
 def connection():
     mongo_uri = os.getenv("MONGO_URI")
     mongo_db = os.getenv("MONGO_DB")
-    
     client = MongoClient(mongo_uri)
     db = client[mongo_db]
 
